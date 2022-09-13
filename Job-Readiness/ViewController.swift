49ffd4c58228691a3b920c12739374440bc882a1
Berchon <berchon@gmail.com>
2022-09-13 19:52:16 -0300

    Initial Commit

---
+++ Job-Readiness/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Job-Readiness
+//
+//  Created by Luciano Da Silva Berchon on 13/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
